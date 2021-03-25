bc2836bf081b6f954939800fe284d8ccc4d6a06b
Blacker <oleg_frolov@mail.ru>
2021-03-25 23:52:55 +0300

    update

--- forms/subject/_class.php
+++ forms/subject/_class.php
@@ -1,6 +1,14 @@
 <?php
 class subjectClass extends cmsFormsClass {
 
+    function afterItemRead(&$item) {
+        isset($item['_created']) && !isset($item['created']) ? $item['created'] = $item['_created'] : null;
+        isset($item['created']) && !isset($item['_created']) ? $item['_created'] = $item['created'] : null;
+    }
+
+    function beforeItemShow(&$item) {
+        $item['created'] = date('d.m.Y H:i', strtotime($item['created']));
+    }
 
 }
 ?>
